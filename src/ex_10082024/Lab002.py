b6b594132cf1dcc3ed261d85fd2f3acc1f7d7de5
Anshuman Dash <andash@fct.ca>
2024-08-12 14:56:56 -0400

    lab001 commit

--- src/ex_10082024/Lab002.py
+++
@@ -1,7 +0,0 @@
-print("Hello world!")
-
-print("Hello", "World", "Amit", "pramod", 123, 3.13, True, sep="-")
-print("Hello", "World", "Amit", "pramod", 123, 3.13, True)
-print("Hello", "World", "Amit", "pramod", 123, 3.13, True, sep="|")
-print("Hello", "World", "Amit", "pramod", 123, 3.13, True, sep="|", end="_")
-print("Anshuman")
